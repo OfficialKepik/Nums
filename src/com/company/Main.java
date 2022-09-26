ecc1f361b50e2f602f57581a31574c479e87a6c9
OfficialKepik <officialkepik@gmail.com>
2022-09-26 18:58:50 +0300

    Create project

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int points = 20;
+        if (points < 30) System.out.println("your mark is 3");
+        else if (points < 50) System.out.println("Your mark is 5");
+        else System.out.println("Your mark is 5");
+    }
+}
